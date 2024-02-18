5a4ad7dd735b749894b3e5f2e58b2ac16af2bc18
ilmanvillvay <ilman@villvay.com>
2024-02-18 23:02:40 +0530

    fix mysql connection string in properties file

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM openjdk:11-jre-slim
 
 ENV PORT 8080
+ENV CUSTOM_VARIABLE 'value set from dockerfile'
 
-# ENV DB_HOST 127.0.0.1:3307
-# ENV DB_NAME customer_service_db
-# ENV DB_USERNAME root
-# ENV DB_PASSWORD 123456
+WORKDIR /app
+
+COPY ./target/customer-service-0.0.1-SNAPSHOT.jar customer-service.jar
+
+VOLUME ./logs
 
 EXPOSE $PORT
 
-COPY target/customer-service-0.0.1-SNAPSHOT.jar customer-service.jar
-
-CMD ["java", "-jar", "/customer-service.jar"]
+CMD ["java", "-jar", "/app/customer-service.jar"]
--- src/main/java/com/milmaniq/customer/controller/CustomerController.java
+++ src/main/java/com/milmaniq/customer/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.milmaniq.customer.controller;
 import com.milmaniq.customer.dto.CustomerDto;
 import com.milmaniq.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,15 @@ import java.util.List;
 public class CustomerController {
 
     private final CustomerService customerService;
+
+    @Value("${app.custom-variable}")
+    private String customVariable;
+
+    @GetMapping("/variable-value")
+    public ResponseEntity<String> getValueOfCustomVariable() {
+        final String value = "The value of the custom variable is: " + this.customVariable;
+        return ResponseEntity.ok(value);
+    }
 
     @GetMapping()
     public List<CustomerDto> getAllCustomers() {
